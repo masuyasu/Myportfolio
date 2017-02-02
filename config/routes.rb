246b3f4549e22f547aa92f6af1142e2353c773a9
masudayasushi1008 <vpsz-yasu.10@ozzio.jp>
2017-02-02 14:20:51 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#top'
+
+  get 'home/profile' => 'home#profile'
+
+  get 'home/work' => 'home#work'
+
+  get 'home/contact' => 'home#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
